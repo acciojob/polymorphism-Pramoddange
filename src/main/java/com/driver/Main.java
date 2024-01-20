2a70f8935fbad060639f2bc1fbcd33728f1d4282
Pramod Dange <pramodsdange1@gmail.com>
2024-01-21 02:02:55 +0530

    polmorphism is completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int product(int x, int y) {
+              return 0;
+        }
+
+
+            public int product(int x, int y, int z) {
+                 return 0;
+            }
+
+        public double product(double x, double y) {
+          return 2.3;
+        }
+    }
+
+
+    public static void main(String[] args){
+        Product p=new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.3,3.5);
+    }
 
 }
